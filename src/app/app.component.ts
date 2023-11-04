e792e9998adeadd79aac199db77a32a7fd5f3f2b
dinesh <dinesh.bharti.be16@gmail.com>
2023-11-04 05:12:22 -0700

    two-way bindind work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'registrationForm';
   Courses=['Python' ,'Java', 'C++','Php','JavaScript','Angulor', 'React'];
+  StudentObj=new Student("dinesh","dinesh@gmail.com",8817048018,"python","Evening",true,"Satna")
 }
---
+++ src/app/student.ts
@@ -0,0 +1,14 @@
+export class Student {
+    constructor(
+        public Name :string,
+        public Email :string,
+        public Phone :number,
+        public Course :string,
+        public Time :string,
+        public subscribe :boolean,
+        public City :string
+       
+
+    )
+    {}
+}
